fb7476a796959e438ef4a05c9cffa27cc6878f7c
androidcompanion <nikunj.dhami392@gmail.com>
2021-09-23 15:33:16 +0530

    Initial commit

---
+++ app/src/main/java/com/adscomapanion2021/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.adscomapanion2021;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+
+import com.newAds2021.adsmodels.ConstantAds;
+import com.newAds2021.adutils.BaseAdsClass;
+
+
+public class BaseActivity extends BaseAdsClass {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ConstantAds.setAdsUrlID("https://script.google.com/macros/s/AKfycbwWa0oIwNsZ4b7b-aIGi61iyJ98XFCy2kbfXNC-ZhiIkHtlHu2R88r-gzHc7eigJykh7A/");
+        ConstantAds.setIHAdsID("https://script.google.com/macros/s/AKfycbwV0QJZQ0bg6w_Rqc4w84OFv4cUSI_YZuKKd-exgyFzw-lXSyJ6vbV8-cT1kUBc4MZWwA/");
+//        ConstantAds.setIHInterID("https://script.google.com/macros/s/AKfycbzp0Q7F-wRolDavXSrnTmdsfe8IkLx5nq84C70ZcRNZQHEEKwfxFsRcOntjoEdu78Owyg/");
+//        ConstantAds.setIHNativeID("https://script.google.com/macros/s/AKfycbwMMZbC4a_mbZXTbPNFIma1xy1W1Y9n_ALpJ90do2sHrQ99RaPV2QjZ9FJJSG5iIfQI2g/");
+        ConstantAds.preloadAppopen(true);
+        ConstantAds.preloadInterstitial(true);
+    }
+
+}
